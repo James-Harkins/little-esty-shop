8ca222d841979dcc245c1452564ccce478b077d6
James Harkins <jh.harkins@protonmail.com>
2022-04-22 11:13:53 -0600

    Feat: pass feature tests for merchant_discounts#show page

--- spec/features/merchant_discounts/show_spec.rb
+++ spec/features/merchant_discounts/show_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'merchant_discounts index page' do
   describe 'as a user' do
-    describe 'when i visit my discounts index page' do
+    describe 'when i visit my discounts show page' do
       it 'i see all of my bulk discounts and their percentage and quantity thresholds' do
         merchant_1 = Merchant.create!(name: "Jim's Plates")
         discount_1 = merchant_1.discounts.create!(percentage: 20, quantity_threshold: 10)
@@ -9,9 +9,9 @@ RSpec.describe 'merchant_discounts index page' do
 
         visit "/merchants/#{merchant_1.id}/discounts/#{discount_1.id}"
 
-        expect(page).to have_content("Buy 20 or more items and receive 10% off!")
-        expect(page).not_to have_content("Buy 30 or more items and receive 15% off!")
-        expect(page).not_to have_content("Buy 50 or more items and receive 35% off!")
+        expect(page).to have_content("Buy 10 or more items and receive 20% off!")
+        expect(page).not_to have_content("Buy 15 or more items and receive 30% off!")
+        expect(page).not_to have_content("Buy 35 or more items and receive 50% off!")
       end
     end
   end
