09a030de1bfe04ebe49f7f31813edd6e89eb78db
James Harkins <jh.harkins@protonmail.com>
2022-04-22 09:35:27 -0600

    FeatTest: create tests for merhant_discounts#index page

---
+++ spec/features/merchant_discounts/index_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant_discounts index page' do
+  describe 'as a user' do
+    describe 'when i visit my discounts index page' do
+      it 'i see all of my bulk discounts and their percentage and quantity thresholds' do
+        merchant_1 = Merchant.create!(name: "Jim's Plates")
+        discount_1 = merchant_1.discounts.create!(percentage: 20, quantity_threshold: 10)
+        discount_2 = merchant_1.discounts.create!(percentage: 30, quantity_threshold: 15)
+
+        merchant_2 = Merchant.create!(name: "John's Bars")
+        discount_3 = merchant_2.discounts.create!(percentage: 50, quantity_threshold: 20)
+
+        visit "/merchants/#{merchant_1.id}/discounts"
+
+        expect(page).to have_content(discount_1.percentage)
+        expect(page).to have_content(discount_1.quantity_threshold)
+        expect(page).to have_content(discount_2.percentage)
+        expect(page).to have_content(discount_2.quantity_threshold)
+        expect(page).not_to have_content(discount_3.percentage)
+        expect(page).not_to have_content(discount_3.quantity_threshold)
+      end
+
+      it 'i also see a link to each discount show page' do
+        merchant_1 = Merchant.create!(name: "Jim's Plates")
+        discount_1 = merchant_1.discounts.create!(percentage: 20, quantity_threshold: 10)
+        discount_2 = merchant_1.discounts.create!(percentage: 30, quantity_threshold: 15)
+
+        merchant_2 = Merchant.create!(name: "John's Bars")
+        discount_3 = merchant_2.discounts.create!(percentage: 50, quantity_threshold: 20)
+
+        visit "/merchants/#{merchant_1.id}/discounts"
+
+        within "#discount-#{discount_1.id}" do
+          click_link "More Details"
+        end
+
+        expect(current_path).to eq("/merchants/#{merchant_1.id}/discounts/#{discount_1.id}")
+      end
+    end
+  end
+end
