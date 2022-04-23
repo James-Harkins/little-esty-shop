2646dc27e75941321db6618b23a7e960ad3a00ee
James Harkins <jh.harkins@protonmail.com>
2022-04-22 18:59:21 -0600

    FeatTest: create tests for total revenue and discounted revenue displayed on merchant_invoices#show page

--- spec/features/merchant_invoices/show_spec.rb
+++ spec/features/merchant_invoices/show_spec.rb
@@ -132,5 +132,70 @@ RSpec.describe 'the merchant invoice show page' do
         end
       end
     end
+
+    describe 'applying bulk discounts' do
+      before :each do
+        @merchant_1 = Merchant.create!(name: "Jim's Rare Guitars")
+        @item_1 = @merchant_1.items.create!(name: "1959 Gibson Les Paul",
+                                        description: "Tobacco Burst Finish, Rosewood Fingerboard",
+                                        unit_price: 25000)
+        @item_2 = @merchant_1.items.create!(name: "1954 Fender Stratocaster",
+                                        description: "Seafoam Green Finish, Maple Fingerboard",
+                                        unit_price: 10000)
+        @item_3 = @merchant_1.items.create!(name: "1968 Gibson SG",
+                                        description: "Cherry Red Finish, Rosewood Fingerboard",
+                                        unit_price: 400)
+        @item_4 = @merchant_1.items.create!(name: "1984 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 600)
+        @item_5 = @merchant_1.items.create!(name: "1991 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 900)
+        @item_6 = @merchant_1.items.create!(name: "1993 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 700)
+        @item_7 = @merchant_1.items.create!(name: "2004 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 200)
+        @item_8 = @merchant_1.items.create!(name: "1997 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 100)
+        @item_9 = @merchant_1.items.create!(name: "1996 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 100)
+        @item_10 = @merchant_1.items.create!(name: "1975 Gibson Les Paul",
+                                        description: "Sunburst Finish, Maple Fingerboard",
+                                        unit_price: 400)
+        @customer_1 = Customer.create!(first_name: "Guthrie", last_name: "Govan")
+
+        @invoice_1 = @customer_1.invoices.create!(status: 1)
+        @invoice_2 = @customer_1.invoices.create!(status: 0)
+        @invoice_item_1 = InvoiceItem.create!(item: @item_1, invoice: @invoice_1, quantity: 1, unit_price: 5, status: 0)
+        @invoice_item_2 = InvoiceItem.create!(item: @item_9, invoice: @invoice_1, quantity: 20, unit_price: 10, status: 0)
+        @invoice_item_3 = InvoiceItem.create!(item: @item_2, invoice: @invoice_1, quantity: 10, unit_price: 20, status: 0)
+        @invoice_item_4 = InvoiceItem.create!(item: @item_4, invoice: @invoice_1, quantity: 30, unit_price: 5, status: 0)
+        @invoice_item_5 = InvoiceItem.create!(item: @item_3, invoice: @invoice_1, quantity: 35, unit_price: 10, status: 0)
+        @invoice_item_6 = InvoiceItem.create!(item: @item_5, invoice: @invoice_1, quantity: 10, unit_price: 25, status: 0)
+        @invoice_item_7 = InvoiceItem.create!(item: @item_6, invoice: @invoice_1, quantity: 5, unit_price: 10, status: 0)
+        @invoice_item_8 = InvoiceItem.create!(item: @item_8, invoice: @invoice_1, quantity: 10, unit_price: 15, status: 0)
+        @invoice_item_9 = InvoiceItem.create!(item: @item_7, invoice: @invoice_1, quantity: 1, unit_price: 5, status: 0)
+        @invoice_item_10 = InvoiceItem.create!(item: @item_10, invoice: @invoice_1, quantity: 4, unit_price: 20, status: 0)
+        @invoice_item_11 = InvoiceItem.create!(item: @item_5, invoice: @invoice_2, quantity: 10000, unit_price: 25, status: 0)
+        @invoice_item_12 = InvoiceItem.create!(item: @item_7, invoice: @invoice_2, quantity: 10000, unit_price: 50, status: 0)
+        @invoice_item_13 = InvoiceItem.create!(item: @item_8, invoice: @invoice_1, quantity: 10000, unit_price: 20, status: 0)
+        @discount_1 = @merchant_1.discounts.create!(percentage: 20, quantity_threshold: 20)
+        @discount_2 = @merchant_1.discounts.create!(percentage: 30, quantity_threshold: 40)
+
+        visit "/merchants/#{@merchant_1.id}/invoices/#{@invoice_1.id}"
+      end
+
+      it 'i see the total revenue for the invoice' do
+        expect(page).to have_content("Total Revenue: $14.40")
+      end
+
+      it 'i see the discounted revenue for the invoice' do
+        expect(page).to have_content("Revenue after Discounts: $12.00")
+      end
+    end
   end
 end
