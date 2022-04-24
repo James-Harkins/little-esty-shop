9ba31273920d56169fcb5586241aaa05e9d9e8eb
James Harkins <jh.harkins@protonmail.com>
2022-04-23 22:36:47 -0600

    Feat: pass additional edge case tests for the merchant_invoices#show page displays of total and discounted revenue so that other merchants items are not included in calculation

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -11,6 +11,33 @@ class InvoiceItem < ApplicationRecord
     sum('unit_price * quantity')
   end
 
+  def self.total_revenue_for_invoice(invoice_id)
+    sorted_invoices = self.where(invoice_id: invoice_id)
+    sorted_invoices.sum('invoice_items.quantity * invoice_items.unit_price')
+  end
+
+  def self.apply_discounts_for_invoice(invoice_id)
+    sorted_invoice_items = self.where(invoice_id: invoice_id)
+    sorted_invoice_items.each do |invoice_item|
+      if invoice_item.invoice_id == invoice_id
+        invoice_item.discounts.distinct.order(percentage: :desc).each do |discount|
+          if discount.quantity_threshold <= invoice_item.quantity && invoice_item.discount_percentage == 100
+            invoice_item.update(discount_percentage: invoice_item.discount_percentage - discount.percentage)
+          end
+        end
+      end
+    end
+    sorted_invoice_items
+  end
+
+  def self.discounted_revenue_for_invoice(invoice_id)
+    total = 0
+    apply_discounts_for_invoice(invoice_id).each do |invoice_item|
+      total += ((invoice_item.unit_price * invoice_item.quantity) * (invoice_item.discount_percentage.to_f / 100))
+    end
+    total
+  end
+
   def invoice_dates
     invoice.created_at.strftime("%A, %B %d, %Y")
   end
