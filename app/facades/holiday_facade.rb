fbdb8354b7dc2bd4a0416fe95dc09fd88b1720c6
James Harkins <jh.harkins@protonmail.com>
2022-04-25 20:53:56 -0600

    Refactor: update holiday_facade next_three_holidays method

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -8,10 +8,8 @@ class HolidayFacade
   end
 
   def next_three_holidays
-    holidays = []
-    holiday_data[0..2].each do |holiday_sub_data|
-      holidays << Holiday.new(holiday_sub_data)
+    holiday_data[0..2].map do |holiday_sub_data|
+      Holiday.new(holiday_sub_data)
     end
-    holidays
   end
 end
