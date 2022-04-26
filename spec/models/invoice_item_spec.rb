e796cef51e21c6f8b3b34d1704a4cb91cd874456
James Harkins <jh.harkins@protonmail.com>
2022-04-26 10:33:51 -0600

    Refactor: move tests in invoice_item_spec.rb to match order on the model file invoice_item.rb

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -111,49 +111,6 @@ RSpec.describe InvoiceItem, type: :model do
   end
 
   describe 'instance methods' do
-    describe '.invoice_date' do
-      it 'returns the created_at date of the associated invoice' do
-        date_1 = Date.parse("2002-05-01")
-        date_2 = Date.parse("2007-09-01")
-        customer_1 = Customer.create!(first_name: "Guthrie", last_name: "Govan")
-        invoice_1 = customer_1.invoices.create!(status: 1, created_at: date_1)
-        invoice_2 = customer_1.invoices.create!(status: 0, created_at: date_2)
-        merchant_1 = Merchant.create!(name: "Jim's Rare Guitars")
-        item_1 = merchant_1.items.create!(name: "1959 Gibson Les Paul",
-                                        description: "Tobacco Burst Finish, Rosewood Fingerboard",
-                                        unit_price: 25000)
-        item_2 = merchant_1.items.create!(name: "1954 Fender Stratocaster",
-                                        description: "Seafoam Green Finish, Maple Fingerboard",
-                                        unit_price: 10000)
-        invoice_item_1 = InvoiceItem.create!(item: item_1, invoice: invoice_1, quantity: 1, unit_price: 5, status: 0)
-        invoice_item_2 = InvoiceItem.create!(item: item_2, invoice: invoice_2, quantity: 20, unit_price: 10, status: 0)
-
-        expect(invoice_item_1.invoice_date).to eq(date_1)
-        expect(invoice_item_2.invoice_date).to eq(date_2)
-      end
-    end
-
-    describe '.belongs_to_merchant' do
-      it 'returns true if the invoice_item belongs to the merchant passed as an argument' do
-        merchant_1 = Merchant.create!(name: "Jim's Rare Guitars")
-        merchant_2 = Merchant.create!(name: "John's Less Rare Guitars")
-        item_1 = merchant_1.items.create!(name: "1959 Gibson Les Paul",
-                                        description: "Tobacco Burst Finish, Rosewood Fingerboard",
-                                        unit_price: 250000)
-        item_2 = merchant_2.items.create!(name: "2006 Fender Stratocaster",
-                                        description: "Seafoam Green Finish, Maple Fingerboard",
-                                        unit_price: 500)
-        customer_1 = Customer.create!(first_name: "Guthrie", last_name: "Govan")
-        invoice_1 = customer_1.invoices.create!(status: 1)
-        invoice_item_1 = InvoiceItem.create!(item: item_1, invoice: invoice_1, quantity: 1, unit_price: 5, status: 0)
-        invoice_item_2 = InvoiceItem.create!(item: item_2, invoice: invoice_1, quantity: 20, unit_price: 10, status: 0)
-
-        expect(invoice_item_1.belongs_to_merchant?(merchant_1.id)).to eq(true)
-        expect(invoice_item_1.belongs_to_merchant?(merchant_2.id)).to eq(false)
-        expect(invoice_item_2.belongs_to_merchant?(merchant_2.id)).to eq(true)
-        expect(invoice_item_2.belongs_to_merchant?(merchant_1.id)).to eq(false)
-      end
-    end
 
     describe '.applied_bulk_discount' do
       it 'returns the id of the bulk_discount applied to it' do
@@ -299,6 +256,50 @@ RSpec.describe InvoiceItem, type: :model do
         expect(invoice_item_4.discount_multiplier).to eq(0.6)
         expect(invoice_item_5.discount_multiplier).to eq(0.6)
         expect(invoice_item_6.discount_multiplier).to eq(1)
+      end
+    end
+
+    describe '.invoice_date' do
+      it 'returns the created_at date of the associated invoice' do
+        date_1 = Date.parse("2002-05-01")
+        date_2 = Date.parse("2007-09-01")
+        customer_1 = Customer.create!(first_name: "Guthrie", last_name: "Govan")
+        invoice_1 = customer_1.invoices.create!(status: 1, created_at: date_1)
+        invoice_2 = customer_1.invoices.create!(status: 0, created_at: date_2)
+        merchant_1 = Merchant.create!(name: "Jim's Rare Guitars")
+        item_1 = merchant_1.items.create!(name: "1959 Gibson Les Paul",
+          description: "Tobacco Burst Finish, Rosewood Fingerboard",
+          unit_price: 25000)
+        item_2 = merchant_1.items.create!(name: "1954 Fender Stratocaster",
+            description: "Seafoam Green Finish, Maple Fingerboard",
+            unit_price: 10000)
+        invoice_item_1 = InvoiceItem.create!(item: item_1, invoice: invoice_1, quantity: 1, unit_price: 5, status: 0)
+        invoice_item_2 = InvoiceItem.create!(item: item_2, invoice: invoice_2, quantity: 20, unit_price: 10, status: 0)
+
+        expect(invoice_item_1.invoice_date).to eq(date_1)
+        expect(invoice_item_2.invoice_date).to eq(date_2)
+      end
+    end
+
+    describe '.belongs_to_merchant' do
+      it 'returns true if the invoice_item belongs to the merchant passed as an argument' do
+        merchant_1 = Merchant.create!(name: "Jim's Rare Guitars")
+        merchant_2 = Merchant.create!(name: "John's Less Rare Guitars")
+        item_1 = merchant_1.items.create!(name: "1959 Gibson Les Paul",
+          description: "Tobacco Burst Finish, Rosewood Fingerboard",
+          unit_price: 250000)
+        item_2 = merchant_2.items.create!(name: "2006 Fender Stratocaster",
+          description: "Seafoam Green Finish, Maple Fingerboard",
+          unit_price: 500)
+        customer_1 = Customer.create!(first_name: "Guthrie", last_name: "Govan")
+        invoice_1 = customer_1.invoices.create!(status: 1)
+        invoice_item_1 = InvoiceItem.create!(item: item_1, invoice: invoice_1, quantity: 1, unit_price: 5, status: 0)
+        invoice_item_2 = InvoiceItem.create!(item: item_2, invoice: invoice_1, quantity: 20, unit_price: 10, status: 0)
+
+        expect(invoice_item_1.belongs_to_merchant?(merchant_1.id)).to eq(true)
+        expect(invoice_item_1.belongs_to_merchant?(merchant_2.id)).to eq(false)
+        expect(invoice_item_2.belongs_to_merchant?(merchant_2.id)).to eq(true)
+        expect(invoice_item_2.belongs_to_merchant?(merchant_1.id)).to eq(false)
       end
     end
   end
